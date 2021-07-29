9cf5e89fe2400402b54fbbfe6edcd9a69a1ab72f
LiliAmber <dmraraa@gmail.com>
2021-07-29 21:15:20 +0700

    remove counter

--- leaflet-test/src/components/Markers.jsx
+++ leaflet-test/src/components/Markers.jsx
@@ -1,28 +1,12 @@
-import React, { useState, useMemo } from "react";
-import { Marker, Popup, Tooltip } from "react-leaflet";
+import React from "react";
+import { Marker, Popup } from "react-leaflet";
 export default function Markers(props) {
-  //===Set the initial count state to zero, 0====
-  const [clickedCount, setClickedCount] = useState(0);
-  //===Create handleIncrement event handler===
-  const eventHandlers = useMemo(
-    () => ({
-      click() {
-        setClickedCount((count) => count + 1);
-      },
-    }),
-    []
-  );
   return (
     <>
       {props.hospitals.map((item) => {
         return (
-          <Marker
-            eventHandlers={eventHandlers}
-            key={item.id}
-            position={item.coordinates}
-          >
-            <Tooltip>{item.name}</Tooltip>
-            <Popup>You clicked marker: {clickedCount}</Popup>
+          <Marker key={item.id} position={item.coordinates}>
+            <Popup>You clicked marker: {item.name}</Popup>
           </Marker>
         );
       })}
